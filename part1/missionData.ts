67c5fcbde78f4d7d4bb2c39200b85d160fd9895f
Ethan_Selleron <ethan.selleron@epitech.eu>
2021-03-08 16:13:49 +0100

    [ADD] : exo1 and exo2

---
+++ part1/missionData.ts
@@ -0,0 +1,8 @@
+const operationCode: number = 42;
+const operationAllowed: boolean = true;
+const operationName: string = "KSW";
+const agents: { name?: string; code: number }[] = [
+  { name: "Kreog", code: 42 },
+  { name: "Mate", code: 21 },
+  { code: 0.07 },
+];
